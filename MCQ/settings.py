39ca2419d4c093ec668913ee4bcb39d24b063192
Ram <ramprasathdgl@gmail.com>
2014-11-12 03:09:04 +0530

    Add support for handling Static files like CSS and js
    Configured STATIC_ROOT and MEDIA_ROOT ,
    Ran python manage.py collectstatic to collect all the static files
    and put in the STATIC_ROOT

--- MCQ/settings.py
+++ MCQ/settings.py
@@ -82,3 +82,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(BASE_DIR,
+                               "static", "static-only")
+    MEDIA_ROOT = os.path.join(BASE_DIR,
+                              "static", "media")
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "static", "static"),
+    )
--- MCQ/urls.py
+++ MCQ/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'MCQ.views.home', name='home'),
@@ -9,3 +12,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^quiz/', include('quiz.urls', namespace="quiz")),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
