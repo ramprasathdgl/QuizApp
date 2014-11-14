875e158cce77f83f764675e72fb0c814efb9cbe3
Ram <ramprasathdgl@gmail.com>
2014-11-14 22:46:40 +0530

    Add UserDetail model to models.py

--- quiz/models.py
+++ quiz/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+TITLE_CHOICES = (
+    ('MR', 'Mr.'),
+    ('MRS', 'Mrs.'),
+    ('MS', 'Ms.'),
+)
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=100)
     answer_text = models.CharField(max_length=100)
@@ -14,3 +21,13 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class UserDetail(models.Model):
+    title = models.CharField(max_length=3, choices=TITLE_CHOICES)
+    name = models.CharField(max_length=100)
+    email_id = models.EmailField(max_length=70, unique=True)
+    birth_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
