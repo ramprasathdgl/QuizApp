505ba55f8ab551b7bafef2cb47093887fe086e6c
Ram <ramprasathdgl@gmail.com>
2014-11-25 21:22:36 +0530

    removed redundant entries

--- MCQ/settings.py
+++ MCQ/settings.py
@@ -83,12 +83,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    MEDIA_URL = '/media/'
-    STATIC_ROOT = os.path.join(BASE_DIR,
-                               "static", "static-only")
-    MEDIA_ROOT = os.path.join(BASE_DIR,
-                              "static", "media")
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, "static", "static"),
-    )
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR,
+                          "static", "media")
