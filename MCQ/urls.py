3eb164cc42772f9e5833e0e2b749e99c1b8f7e1a
Ram <ramprasathdgl@gmail.com>
2014-11-30 11:08:18 +0530

    Removed redundant lines from urls.py

--- MCQ/urls.py
+++ MCQ/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,8 +14,8 @@ urlpatterns = patterns('',
     url(r'', include('homepage.urls', namespace="homepage")),
 )
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#    urlpatterns += static(settings.STATIC_URL,
+#                          document_root=settings.STATIC_ROOT)
+#    urlpatterns += static(settings.MEDIA_URL,
+#                          document_root=settings.MEDIA_ROOT)
